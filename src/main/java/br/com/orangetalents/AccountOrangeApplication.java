b95b3fbc68625edb4447863ad24bcd03b30ea01d
Ezequiel Oliveira <zeque2020@gmail.com>
2021-01-04 20:03:22 -0200

    primeiro commit: Inicia Projeto

---
+++ src/main/java/br/com/orangetalents/AccountOrangeApplication.java
@@ -0,0 +1,13 @@
+package br.com.orangetalents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountOrangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountOrangeApplication.class, args);
+	}
+
+}
