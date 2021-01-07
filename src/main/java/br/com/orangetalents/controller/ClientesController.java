fe2031816291969902b6f37b4e69259cbd15c250
EzequielOS <zeque2020@gmail.com>
2021-01-07 16:21:58 -0200

    Aplica status CREATED como retorno

--- src/main/java/br/com/orangetalents/controller/ClientesController.java
+++ src/main/java/br/com/orangetalents/controller/ClientesController.java
@@ -1,10 +1,12 @@
 package br.com.orangetalents.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.orangetalents.model.Cliente;
@@ -22,6 +24,7 @@ public class ClientesController {
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente cadastro(@RequestBody Cliente cliente) {
 	
 		return clienteRepository.save(cliente);
